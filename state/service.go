1a447f9b469fb26d60fd1f71b8296bf7dc01723c
Бачериков Никита <n.bacherikov@simcord.com>
2025-12-01 18:19:59 +0200

    wip

--- state/service.go
+++ state/service.go
@@ -18,11 +18,15 @@ type HandlerInfo struct {
 
 type TelegramStateService[Action storage.UserAction, Command string, Callback string] struct {
 	chatRequestChannels map[int64]chan tgbotapi.Update
+	allowedUpdateIDs    map[int64]int
 	processingQueueChan chan int64
 
 	commandHandler  map[Command]HandlerInfo
 	actionHandler   map[Action]HandlerInfo
 	callbackHandler map[Callback]HandlerInfo
+
+	chatMemberHandler   HandlerFunc
+	meChatMemberHandler HandlerFunc
 
 	actionStorage *storage.UserActionStorage[Action]
 }
@@ -56,11 +60,66 @@ func (t *TelegramStateService[Action, Command, Callback]) startConsumeQueueChan(
 }
 
 func (t *TelegramStateService[Action, Command, Callback]) handleUpdate(ctx context.Context, update *tgbotapi.Update) {
+	if update.Message != nil {
+		t.handleMessage(ctx, update)
+	}
 
+	if update.CallbackQuery != nil {
+		t.handleCallback(ctx, update)
+	}
+
+}
+
+func (t *TelegramStateService[Action, Command, Callback]) handleCallback(ctx context.Context, update *tgbotapi.Update) {
+	ctx = wrapper.FillCtx(ctx, update.FromChat().ID, update.SentFrom().ID)
+
+	if lastUpdate, ok := t.allowedUpdateIDs[update.FromChat().ID]; ok {
+		if lastUpdate > update.UpdateID {
+			return
+		}
+	}
+
+	t.allowedUpdateIDs[update.FromChat().ID] = update.UpdateID
+
+	callbackHandler, ok := t.callbackHandler[Callback(update.CallbackData())]
+
+	if ok {
+		callbackHandler.Handler(ctx, update)
+		return
+	}
+
+	action, err := t.actionStorage.GetAction(ctx)
+
+	if err != nil {
+		return
+	}
+
+	actionHandler, ok := t.actionHandler[action]
+
+	if !ok {
+		return
+	}
+
+	actionHandler.Handler(ctx, update)
 }
 
 func (t *TelegramStateService[Action, Command, Callback]) handleMessage(ctx context.Context, update *tgbotapi.Update) {
 	ctx = wrapper.FillCtx(ctx, update.FromChat().ID, update.SentFrom().ID)
+
+	if lastUpdate, ok := t.allowedUpdateIDs[update.FromChat().ID]; ok {
+		if lastUpdate > update.UpdateID {
+			return
+		}
+	}
+
+	t.allowedUpdateIDs[update.FromChat().ID] = update.UpdateID
+
+	cmdHandler, ok := t.commandHandler[Command(update.Message.Command())]
+
+	if ok {
+		cmdHandler.Handler(ctx, update)
+		return
+	}
 
 	action, err := t.actionStorage.GetAction(ctx)
 
